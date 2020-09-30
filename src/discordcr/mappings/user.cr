a74132b0e327d2e19cc02a4dac57995d0f61ba33
GeopJr <geopjr@geopjr.xyz>
2020-09-30 22:40:12 +0300

    Add new UserFlags (https://github.com/discordcr/discordcr/pull/216)

--- src/discordcr/mappings/user.cr
+++ src/discordcr/mappings/user.cr
@@ -56,16 +56,19 @@ module Discord
 
   @[Flags]
   enum UserFlags : UInt32
-    DiscordEmployee = 1 << 0
-    DiscordPartner  = 1 << 1
-    HypeSquadEvents = 1 << 2
-    BugHunter       = 1 << 3
-    HouseBravery    = 1 << 6
-    HouseBrilliance = 1 << 7
-    HouseBalance    = 1 << 8
-    EarlySupporter  = 1 << 9
-    TeamUser        = 1 << 10
-    System          = 1 << 12
+    DiscordEmployee           = 1 << 0
+    PartneredServerOwner      = 1 << 1
+    HypeSquadEvents           = 1 << 2
+    BugHunterLevel1           = 1 << 3
+    HouseBravery              = 1 << 6
+    HouseBrilliance           = 1 << 7
+    HouseBalance              = 1 << 8
+    EarlySupporter            = 1 << 9
+    TeamUser                  = 1 << 10
+    System                    = 1 << 12
+    BugHunterLevel2           = 1 << 14
+    VerifiedBot               = 1 << 16
+    EarlyVerifiedBotDeveloper = 1 << 17
 
     def self.new(pull : JSON::PullParser)
       UserFlags.new(pull.read_int.to_u32)
